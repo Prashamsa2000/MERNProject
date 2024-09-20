707dbe1fed0ff9d6164f3c015f5de2a74fcc2fa5
Prashamsa Shrestha <prashamsashrestha28@gmail.com>
2024-09-20 12:14:57 +0545

    navbarr

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = (props) => {
     const context= useContext(blogContext)
      const {state:{cart}}= context
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-body-tertiary bg-dark">
   <div className="container-fluid">
-    
+  <img src={logo} width="67" height="50"></img>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    <img src={logo} width="67" height="50"></img>
+    
 
     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
     
@@ -37,20 +37,22 @@ const Navbar = (props) => {
                         <li className="nav-item">
                             <Link className="nav-link text-light" to="/contactus">Contact Us</Link>
                         </li>
-                        <Link to="./Signup"><button type="button" className="btn btn-outline-light position-relative ms-2">
+                        <li className="nav-item d-flex flex-lg-row flex-column align-items-lg-center">
+                        <Link to="./Signup"><button type="button" className="btn btn-outline-light my-2 my-lg-0 mx-lg-2">
                             Sign Up
                             
                         </button>
                         </Link>
                         
-                        <Link to="./Login"><button type="button" className="btn btn-outline-light position-relative ms-2">
+                        
+                        <Link to="./Login"><button type="button" className="btn btn-outline-light my-2 my-lg-0 mx-lg-2">
                             Login
                             
                         </button>
                         
                         </Link>
                        
-                        <Link to="./cartitems"><button type="button" className="btn btn-light position-relative ms-3">
+                        <Link to="./cartitems"><button type="button" className="btn btn-light position-relative my-2 my-lg-0 mx-lg-2">
                             My Cart<FaShoppingCart className='ms-2'/>
                             <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                                 {cart.length}
@@ -58,7 +60,7 @@ const Navbar = (props) => {
                             </span>
                         </button>
                         </Link>
-
+</li>
                     </ul>
 
     </div>
